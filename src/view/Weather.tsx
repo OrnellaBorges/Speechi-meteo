7d3c7689d4f830e57152012ca947a216bf2d19cd
Ornella B <borges.ornella@gmail.com>
2024-05-27 18:22:53 +0200

    save

--- src/view/Weather.tsx
+++ src/view/Weather.tsx
@@ -12,16 +12,19 @@ export function Weather() {
   console.log("datas =>>", weatherInfos);
   return (
     <>
-      <h1>WEATHER</h1>
-      {weatherInfos ? (
-        <>
-          <p>Ville: {weatherInfos.name}</p>
-          <p>Temperature: {weatherInfos.main.temp}</p>
-          <p>Latitude: {clientCoords.latitude}</p>
-          <p>Longitude: {clientCoords.longitude}</p>
-        </>
-      ) : (
-        <p>No weather data available</p>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {weatherInfos && (
+        <div className="weatherContainer">
+          <h2 className="weatherCity">{weatherInfos.name}</h2>
+          <p className="date">Date: {new Date().toLocaleDateString()}</p>
+          <p className="temperature">{weatherInfos.main.temp}°C</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weatherInfos.weather[0].icon}.png`}
+            alt={weatherInfos.weather[0].description}
+          />
+          <p>{weatherInfos.weather[0].description}</p>
+        </div>
       )}
     </>
   );
