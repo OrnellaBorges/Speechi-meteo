bdc129031f8709850a1068c14b88f3e370c3027a
Ornella B <borges.ornella@gmail.com>
2024-06-02 21:28:34 +0200

    save utils completed

--- src/utils/cacheUtils.ts
+++ src/utils/cacheUtils.ts
@@ -1,16 +1,17 @@
+import { time, timeStamp } from "console";
 import { CoordsType } from "../types/CoordsType";
 import { WeatherResponse } from "../types/WeatherTypes";
 
 type CachedWeatherDatas = {
-  cachecontent: WeatherResponse;
+  cacheContent: WeatherResponse;
   timestamp: number;
 };
 
 // 10 min expiration du cache
 const CACHE_EXPIRATION_TIME = 10 * 60 * 1000;
 
-//fonction qui permet de creer une key pour le cache
-function generateCacheKey(coords: CoordsType): String {
+//fonction qui permet de creer une key pour le cache besoins des coords en params
+function generateCacheKey(coords: CoordsType): string {
   const { latitude, longitude } = coords; // destructuration de l'objet coords
   const cacheKey = `weather_${latitude}_${longitude}`; // creation clé unique de cache
   return cacheKey;
@@ -38,19 +39,33 @@ function checkCacheDataExist(): boolean {
 }
 
 //Creation du cache Si on a verifié qu'il n'y a pas de cache et qu'on a fetch la data de l'api
-function createCacheDatas(
+export function createCacheDatas(
   coords: CoordsType,
   apiResponseValue: WeatherResponse
 ) {
   const key = generateCacheKey(coords); // creation de la Key
-  const cacheValue = apiResponseValue;
+  const cacheValue: CachedWeatherDatas = {
+    cacheContent: apiResponseValue,
+    timestamp: Date.now(),
+  };
+  console.log("cacheValue", cacheValue);
+  console.log("key", key);
+
+  return localStorage.setItem(key, JSON.stringify(cacheValue));
 }
 
 // RECUP LES DATAS DU CACHE SI CACHE EXISTE
 
+// fonction qui recup les données du cache avec la cacheKey
+export function getCachedDatas(cacheKey: string): WeatherResponse {
+  const cachedDatas = localStorage.getItem(cacheKey)!; // ! dit a TS que c'est sure que ce sera non null
+  const parsedDatas = JSON.parse(cachedDatas); // je convertit la data pour la lire en tableau d'objet par Js
+  return parsedDatas;
+}
+
 //fonction pour voir si le cache est expired
 //Si le Cache existe on regarde si il a expiré ou pas !
-function isCacheExpired(cachedDatas: CachedWeatherDatas): boolean {
+export function isCacheExpired(cachedDatas: CachedWeatherDatas): boolean {
   console.log("Is expired ? : ");
   if (!cachedDatas) return false; // si le cache existe pas
 
@@ -58,11 +73,4 @@ function isCacheExpired(cachedDatas: CachedWeatherDatas): boolean {
   const timestamp = cachedDatas.timestamp;
   const currentTime = Date.now(); //recup date actuelle
   return currentTime - timestamp < CACHE_EXPIRATION_TIME; // verif si c'est expiré
-}
-
-// fonction qui recup les données du cache avec la cacheKey
-function getCachedDatas(cacheKey: string): WeatherResponse {
-  const cachedDatas = localStorage.getItem(cacheKey)!; // ! dit a TS que c'est sure que ce sera non null
-  const parsedDatas = JSON.parse(cachedDatas); // je convertit la data pour la lire en tableau d'objet par Js
-  return parsedDatas;
 }
